66afa29956e7198ccbab97f0ebb1db741fdecc04
Luis-Personal <luisdfm03@gmail.com>
2024-01-16 08:35:10 +0000

    Auto Install in tree sitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "javascript", "markdown" },
+  auto_install = true,
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
